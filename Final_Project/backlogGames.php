11596a6464ed7a7fafaa40bc010236ac33dc56a8
Jacob Burke <jburke2@ix.cs.uoregon.edu>
2024-06-13 14:07:50 -0700

    working on backlog games page

---
+++ Final_Project/backlogGames.php
@@ -0,0 +1,141 @@
+<?php
+
+include('connectionData.txt');
+
+$conn = mysqli_connect($server, $user, $pass, $dbname, $port)
+or die('Error connecting to MySQL server.');
+
+?>
+
+<html>
+<head>
+  <title>HowLongToFilter</title>
+  <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+        th {
+            background-color: #f2f2f2;
+            text-align: left;
+        }
+  </style>
+  </head>
+  
+  <body bgcolor="white">
+  
+  
+  <hr>
+  
+  
+<?php
+  
+$platformDropdown = $_POST['PlatformDropdown'];
+$sortingDropdown = $_POST['SortingDropdown'];
+
+// $manufact = mysqli_real_escape_string($conn, $manufact);
+// this is a small attempt to avoid SQL injection
+// better to use prepared statements
+
+if ($platformDropdown === 'All') {    
+	if ($sortingDropdown === 'asc_alph') {
+		$query = "SELECT vg.title, na_release, dev_name, pub_name, average, median, fastest, slowest, main, main_sides, completionist FROM video_games vg";
+		$query .= "JOIN speed_runs sr USING(game_num)";
+		$query .= "JOIN completion_times ct USING(game_num)";
+    		$query .= "ORDER BY vg.title ASC;";
+	}
+	elseif ($sortingDropdown === 'dsc_alph') {
+		$query = "SELECT vg.title, na_release, dev_name, pub_name, average, median, fastest, slowest, main, main_sides, completionist FROM video_games vg";
+		$query .= "JOIN speed_runs sr USING(game_num)";
+		$query .= "JOIN completion_times ct USING(game_num)";
+    		$query .= "ORDER BY vg.title DESC;";
+	}
+	elseif ($sortingDropdown === 'asc_rel') {
+		$query = "SELECT vg.title, na_release, dev_name, pub_name, average, median, fastest, slowest, main, main_sides, completionist FROM video_games vg";
+		$query .= "JOIN speed_runs sr USING(game_num)";
+		$query .= "JOIN completion_times ct USING(game_num)";
+    		$query .= "ORDER BY na_release ASC;";
+	}
+	else { // dsc_rel
+		$query = "SELECT vg.title, na_release, dev_name, pub_name, average, median, fastest, slowest, main, main_sides, completionist FROM video_games vg";
+		$query .= "JOIN speed_runs sr USING(game_num)";
+		$query .= "JOIN completion_times ct USING(game_num)";
+    		$query .= "ORDER BY na_release DESC;";
+
+	}
+}
+
+?>
+
+<p>
+The query:
+<p>
+<?php
+print $query;
+?>
+
+<hr>
+<p>
+Result of query:
+<p>
+
+<?php
+//$result = mysqli_query($conn, $query)
+//or die(mysqli_error($conn));
+
+?>
+<!---
+<table>
+        <tr>
+            <?php
+            // Fetch field names and create table headers
+            if ($result->num_rows > 0) {
+                $fields = $result->fetch_fields();
+                foreach ($fields as $field) {
+                    echo "<th>" . htmlspecialchars($field->name) . "</th>";
+                }
+            }
+            ?>
+        </tr>
+        <?php
+        // Output data of each row
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                foreach ($row as $cell) {
+                    echo "<td>" . htmlspecialchars($cell) . "</td>";
+                }
+                echo "</tr>";
+            }
+        } else {
+            echo "<tr><td colspan='" . count($fields) . "'>No results found</td></tr>";
+        }
+        ?>
+</table>
+-->
+
+
+
+// print "<pre>";
+
+// if ($platformDropdown === 'All') {    
+//	while($row = mysqli_fetch_array($result, MYSQLI_BOTH)) {
+//		print "\n";
+//		print "$row[fname]  $row[lname] $row[description]";
+	}
+//print "</pre>";
+
+<?php
+//mysqli_free_result($result);
+
+//mysqli_close($conn);
+
+?>
+ 
+</body>
+</html>
+	  
